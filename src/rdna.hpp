b7cf1f1e2a82edc1b81156f248d1247cba5b9fef
WaydeBelgrave <WaydeBelgrave@users.noreply.github.com>
2019-02-13 12:11:26 -0400

    Lab 2

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -8,7 +8,6 @@ string randDNA(int seed, string bases, int n)
 {
 	/* Variable for result */
 	string result="";
-	random_device rd;
 	mt19937 eng1(seed);
 	uniform_int_distribution<> unifrm(0, bases.size());
 	for(int i = 0; i <= n; i++)
