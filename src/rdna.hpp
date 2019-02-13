3048d95d00c6d0c91fd27ddee3c8ae45f8af2ad7
WaydeBelgrave <WaydeBelgrave@users.noreply.github.com>
2019-02-13 12:09:22 -0400

    Lab 2

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -7,13 +7,13 @@ using namespace std;
 string randDNA(int seed, string bases, int n)
 {
 	/* Variable for result */
-	string result;
+	string result="";
 	random_device rd;
 	mt19937 eng1(seed);
-	uniform_int_distribution<> unifrm(0, n);
+	uniform_int_distribution<> unifrm(0, bases.size());
 	for(int i = 0; i <= n; i++)
 	{
-		result = bases[i];
+		result = result + bases[unifrm(eng1)];
 	}
 	return result;
 	/* End of Function */
